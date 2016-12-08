Similarity function added - Brush normalization fixed

src/agent.py
@@ -1,9 +1,9 @@
-'''
+"""
 .. py:module:: mas_memory
     :platform: Unix
 An example of an agent's memory model and how an agent can learn (learn here is
 used in a very weak sense) new artifacts to it from domain.
-'''
+"""
 
 from creamas.core import CreativeAgent, Environment, Simulation, Artifact
 
@@ -24,35 +24,35 @@ logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
 
 
-class ListMemory():
-    '''Simple list memory which stores all seen artifacts as is into a list.
-    '''
+class ListMemory:
+    """Simple list memory which stores all seen artifacts as is into a list.
+    """
     def __init__(self, capacity):
-        '''
+        """
         :param int capacity: The maximum number of artifacts in the memory.
-        '''
+        """
         self._capacity = capacity
         self._artifacts = []
 
     @property
     def capacity(self):
-        '''The maximum number of artifacts in the memory.
-        '''
+        """The maximum number of artifacts in the memory.
+        """
         return self._capacity
 
     @property
     def artifacts(self):
-        '''The artifacts currently in the memory.
-        '''
+        """The artifacts currently in the memory.
+        """
         return self._artifacts
 
     def memorize(self, artifact):
-        '''Memorize an artifact into the memory.
+        """Memorize an artifact into the memory.
         If the artifact is already in the memory, does nothing. If memory
         is full and a new artifact is memorized, forgets the oldest artifact.
         :param artifact: Artifact to be learned.
         :type artifact: :class:`~creamas.core.artifact.Artifact`
-        '''
+        """
         if artifact in self._artifacts:
             return
 
@@ -62,113 +62,116 @@ class ListMemory():
 
 
 class FoolPainterAgent(CreativeAgent):
-    '''A sample agent implementation.
+    """A sample agent implementation.
     Agent invents new strokes by combination of brushes patterns and palette of colours that it has learned
     from a given artwork or randomly otherwise.
-    '''
+    """
 
     def __init__(self, env, reference):
-        '''
+        """
         :param env:
             subclass of :py:class:`~creamas.core.environment.Environment`
-        '''
+        """
 
         super().__init__(env)
 
         self.mem = ListMemory(20)
+        self.n = 1
 
         self.color_reference     = create_model(reference)
         self.color_palette_size  = 30
-        self.color_palette       = np.array(self.pickColors(self.color_palette_size))
+        self.color_palette       = np.array(self.pick_colors(self.color_palette_size))
 
-        self.brush_size          = random.randint(2,8) # Grid of NxN ; N = random value from 3 to 5.
+        self.brush_size          = random.randint(3,5) # Grid of NxN ; N = random value from 3 to 5.
         self.brush               = Brush(self.brush_size, reference)
 
         plt.imshow(self.color_palette, interpolation='None')
-        #plt.show()
 
         plt.imshow(self.brush.pattern, interpolation='None', cmap='gray')
-        #plt.show()
 
-    def pickColors (self, n):
+        logger.debug("Agent started: {}".format(reference))
+
+    def pick_colors (self, n):
         # Return n most frequent colors from the color_palette.
         return np.array([sorted(self.color_reference, key=self.color_reference.get)[:n]])
 
-
     def evaluate(self, artifact):
-        '''Evaluate given artifact with respect to the agent's  memory
+        """Evaluate given artifact with respect to the agent's  memory
         and how useful it if for the assigned job.
         :param artifact: :class:`~creamas.core.Artifact` to be evaluated
         :returns:
             (evaluation, framing)-tuple, the framing is the combined framing of
             both the value and novelty.
-        '''
+        """
 
         value, value_framing     = self.value(artifact)
         novelty, novelty_framing = self.novelty(artifact)
-        surpris, surpris_framing = self.surpris(artifact)
+        surpris, surpris_framing = self.surprisingness(artifact)
 
         framing = {'value': value_framing, 'novelty':novelty_framing, 'suprisingness' : surpris_framing}
         evaluation = (value + novelty + surpris) / 3
         return evaluation, framing
 
     def value(self, artifact):
-        '''Compute the value of a given artifact with respect to the stroke the
+        """Compute the value of a given artifact with respect to the stroke the
         agent has in its vocabulary.
 
         :param artifact: :class:`~creamas.core.Artifact` to be evaluated
         :returns:
             (evaluation, stroke)-tuple, containing both the evaluation and the
             stroke giving the maximum evaluation
-        '''
+        """
 
         value, matching_stroke = 0, None;
         return value, matching_stroke
 
     def novelty(self, artifact):
-        '''Compute the novelty of a given artifact with respect to the artifacts
+        """Compute the novelty of a given artifact with respect to the artifacts
         in the agent's memory.
         :param artifact: :class:`~creamas.core.Artifact` to be evaluated
         :returns:
             (novelty, word)-tuple, containing both the novelty value and the
             word giving the minimum novelty.
-        '''
+        """
 
         novelty, matching_stroke = 0, None;
         return novelty, matching_stroke
 
     def surprisingness(self, artifact):
-        '''Compute the surprisingness of a given artifact with respect to the artifacts
+        """Compute the surprisingness of a given artifact with respect to the artifacts
         in the agent's memory.
         :param artifact: :class:`~creamas.core.Artifact` to be evaluated
         :returns:
             (novelty, word)-tuple, containing both the surprisingness value and the
             stroke giving the minimum surprisingness.
-        '''
+        """
 
-        surpris, matching_stroke = 0, None
+        surpris, matching_stroke = 0, None;
         return surpris, matching_stroke
 
     def similarity(self, imgA, imgB):
-        '''Given two images of the same size, this function compute the similarity of the pixel
+        """Given two images of the same size, this function compute the similarity of the pixel
         values. The function compute the differences of RGB values of a pixel and weight it with
         the alpha value.
 
         :param imgA:
         :param imgB:
         :return:
-        '''
+        """
 
+        delta_R = np.multiply(imgA[:,:,3], imgA[:,:,0]) - np.multiply(imgB[:,:,3], imgB[:,:,0])
+        delta_G = np.multiply(imgA[:,:,3], imgA[:,:,1]) - np.multiply(imgB[:,:,3], imgB[:,:,1])
+        delta_B = np.multiply(imgA[:,:,3], imgA[:,:,2]) - np.multiply(imgB[:,:,3], imgB[:,:,2])
 
+        delta = (np.absolute(delta_R) + np.absolute(delta_G) + np.absolute(delta_B)) / 3
 
-
-
+        return (1 - np.mean(delta))
 
     def generate(self):
-        '''Generate a new stroke.
+        """Generate a new stroke.
         Stroke are generated mixing data memorized of patterns and colors.
         :returns: a stroke wrapped as :class:`~creamas.core.artifact.Artifact`
-        '''
+        """
         random_color = random.choice(self.color_palette[0])
         stroke = []
 
@@ -180,21 +183,21 @@ class FoolPainterAgent(CreativeAgent):
 
         stroke = np.array(stroke)
 
-        # Dbg
+        # # Dbg
         plt.imshow(stroke, interpolation='None')
         plt.show()
+
         return Artifact(self, stroke, domain=str)
 
-
     def invent(self, n=20):
-        '''Invent a new stroke.
+        """Invent a new stroke.
         Generates multiple (n) strokes and selects the one with the highest
         evaluation.
         :param int n: Number of strokes to consider
         :returns:
             a stroke wrapped as :class:`~creamas.core.artifact.Artifact` and its
             evaluation.
-        '''
+        """
         best_artifact = self.generate()
         max_evaluation, framing = self.evaluate(best_artifact)
         for _ in range(n-1):
@@ -211,10 +214,9 @@ class FoolPainterAgent(CreativeAgent):
         best_artifact.add_eval(self, max_evaluation, fr=framing)
         return best_artifact
 
-
     async def act(self):
-        '''Agent acts by inventing new strokes.
-        '''
+        """Agent acts by inventing new strokes.
+        """
         artifact = self.invent(self.n)
         self.mem.memorize(artifact)
         logger.debug([a.obj for a in self.mem.artifacts])
@@ -225,3 +227,5 @@ if __name__ == "__main__":
     env = Environment.create(('localhost', 5555))
     agent = FoolPainterAgent(env, reference="../media/starring-night.jpg")
     agent.generate()
+
+

src/brush.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 from scipy import ndimage, misc
@@ -6,30 +5,29 @@ from scipy import ndimage, misc
 import numpy as np
 import random
 
+
 def rgb2gray(rgb):
-    ''' Transform RGB image to grayscale version.
-    '''
-    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    r, g, b = rgb[:, :, 0], rgb[:, :, 1], rgb[:, :, 2]
     gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
 
     return gray
 
-class Brush():
-    '''A brush used by agent to paint a picture.
-    '''
 
-    def __init__(self, size = 5, reference = None):
-        '''
+class Brush:
+    """A brush used by agent to paint a picture.
+    """
+
+    def __init__(self, size=5, reference=None):
+        """
         :param int size: Size of a brush in pixels.
         :param string pattern: Shape of a brush.
-        '''
+        """
         self._size 	= size
         self._reference = reference
 
-        self.pattern = self.generatePattern(self._reference, self._size)
+        self.pattern = self.generate_pattern(self._reference, self._size)
 
-    def generatePattern(self, reference, size):
-
+    def generate_pattern(self, reference, size):
         # Import image from reference and convert it to grayscale.
         img = mpimg.imread(reference)
         gray = np.mean(img, -1)
@@ -56,6 +54,7 @@ class Brush():
         return pattern
 
 
-# Test
+
+    # Test
 if __name__ == "__main__":
     brush = Brush(20, "../media/starring-night.jpg")