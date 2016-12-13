ef57a3da112b9a14707e2d0bdb29605991523afc
Smew <bluesmew@gmail.com>
2016-12-13 02:27:42 +0200

    Add command line arguments

--- src/main.py
+++ src/main.py
@@ -3,20 +3,32 @@ import canvas
 import numpy as np
 import random
 import os
+import sys
+import getopt
 
 import matplotlib.pyplot as pl
 
 from creamas.core import Simulation
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-INSPIRATION = PROJECT_ROOT + "/media/"
 
 if __name__ == "__main__":
-
     np.set_printoptions(threshold=np.nan)
 
     # Selects target image.
     target_image = PROJECT_ROOT + "/media/mona_lisa.jpg"
+    inspiration_folder = PROJECT_ROOT + "/media/"
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hi:o:", ["target=", "inspire="])
+    except getopt.GetoptError:
+        print("Error: Bad arguments")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '--target':
+            target_image = arg
+        elif opt == '--inspire':
+            inspiration_folder = arg
 
     # initializes a canvas
     env = canvas.CanvasEnvironment.create(("localhost", 5555))
@@ -24,7 +36,7 @@ if __name__ == "__main__":
 
     # Initialize the agents
     for i in range(3):
-        reference_image = INSPIRATION + random.choice(os.listdir(INSPIRATION))
+        reference_image = inspiration_folder + random.choice(os.listdir(inspiration_folder))
         fool = agent.FoolPainterAgent(env, reference=reference_image)
 
     # Run the simulation
